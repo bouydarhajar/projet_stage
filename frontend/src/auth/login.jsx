8d8e93a828d52c098c1be9c22e8c15ba50579a32
bouydarhajar <hajarbouydar@gmail.com>
2026-02-05 10:50:56 +0100

    Update login.jsx

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -159,9 +159,6 @@ export default function Login(){
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-200"></div>
             </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="px-4 bg-white text-gray-500">Ou continuer avec</span>
-            </div>
           </div>
           
     
